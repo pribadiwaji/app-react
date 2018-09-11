3930b2f7de883bc488251b21277f4d2287019bbb
pribadiaji <pribadiaji@gmail.com>
2018-09-11 11:15:38 +0700

    h1-selamat beraktivitas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Selamat Datang, Pribadi Aji</h1>
+          <h1 className="App-title">Selamat Datang, Pribadi Aji & Selamat Beraktivitas</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
